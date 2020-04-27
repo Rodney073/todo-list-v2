13b0e2faf313986247837193b482799e40b3ca7a
Rodney073 <63658050+Rodney073@users.noreply.github.com>
2020-04-27 14:44:17 +0200

    icons incl.

--- strings.js
+++ strings.js
@@ -1,14 +1,27 @@
 $('#add').on('click', (event) => {
  // $(event.target).text('This button was clicked');
-  $('ul').append("<li>" + todo.value + "</li>")
+  $('ul').append("<li>" + todo.value +'<p><i class="fas fa-thumbs-up"></i><i class="fas fa-times-circle"></i>' + "</li>")
+ 
+ // $('li').append('<i class="fas fa-thumbs-up"></i>')
+  
+  
+  
 }); 
 
+$('.fas fa-thumbs-up').css('color','red');
+//$('i').css('color','red');
 
 $('ul').on('click', 'li', (event) => {
   $(event.target).toggleClass('line');
+});
+
+$('i').on('click', '.fas fa-thumbs-up', (event) => {
+  $(event.target).text('test');
 });
 
 
 /*$('ul').css('margin-left', '150px');
 $('ul').css('text-algin', 'left');*/
 
+
+//$('.innerbox').append('<i class="fas fa-thumbs-up"></i>')
